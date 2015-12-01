83c560f1a6796636518395c9af95a47ed5136777
olivier.moindrot <omoindrot@gmail.com>
2015-12-01 23:51:53 +0100

    Added  for Services. Changed views for hairdressers

--- couptif/www/js/service.js
+++ couptif/www/js/service.js
@@ -1,5 +1,14 @@
+// All the documentation about $resource can be found at: https://docs.angularjs.org/api/ngResource/service/$resource
+
+var host = 'http://api-couptif.herokuapp.com';
+//var host = 'http://localhost:3000';
+
 angular.module('starter.services', ['ngResource'])
 
   .factory('Hairdresser', function ($resource) {
-    return $resource('http://localhost:3000/hairdressers/:hairdresserId.json');
+    return $resource(host+'/hairdressers/:hairdresserId.json');
+  })
+
+  .factory('Service', function ($resource) {
+    return $resource(host+'/hairdressers/:hairdresserId/services/:serviceId.json')
   });
